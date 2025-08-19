33e8f1fe355c0380eb0473b0afe139c6782b8683
kitsakon-ai <plan6094.nn@gmsil.com>
2025-08-19 20:06:46 +0700

    fronted commut

--- app/api/dogs/route.ts
+++ app/api/dogs/route.ts
@@ -3,5 +3,5 @@ import { NextResponse } from "next/server";
 const prisma = new PrismaClient();
 export async function GET() {
   const data = await prisma.dog_infos.findMany();
-  return NextResponse.json({ status: 200, data: data });
+  return NextResponse.json(data);
 }
