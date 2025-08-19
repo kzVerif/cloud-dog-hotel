fec935991fb1a1443ace741a03e431a68a238c20
kzVerif <165546547+kzVerif@users.noreply.github.com>
2025-08-19 22:46:23 +0700

    test

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,60 @@
-import Image from "next/image";
+interface dog {
+  id_dog: number;
+  dog_name: string;
+  breed: string;
+  owner: string;
+}
+export default async function Home() {
+  const data = [{
+    id_dog: 1,
+    dog_name: "Aom",
+    breed: "yed mae",
+    owner: "Plain"
+  },{
+    id_dog: 1,
+    dog_name: "Aom",
+    breed: "yed mae",
+    owner: "Plain"
+  },{
+    id_dog: 1,
+    dog_name: "Aom",
+    breed: "yed mae",
+    owner: "Plain"
+  },{
+    id_dog: 1,
+    dog_name: "Aom",
+    breed: "yed mae",
+    owner: "Plain"
+  },{
+    id_dog: 1,
+    dog_name: "Aom",
+    breed: "yed mae",
+    owner: "Plain"
+  },]
 
-export default function Home() {
   return (
     <>
-    {/* เริ่มเขียนโค้ดในบล็อคนี้ */}
-    <h1 className="text-3xl">hello okok</h1>
+      <h1 className="text-3xl text-center">DOG LIST</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>NAME</th>
+            <th>BREED</th>
+            <th>OWNER</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((dog: dog, index: number) => (
+            <tr key={index}>
+              <td>{dog.id_dog}</td>
+              <td>{dog.dog_name}</td>
+              <td>{dog.breed}</td>
+              <td>{dog.owner}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </>
   );
 }
