11294c23d6dbaf4ef403333daefe9015ec0b549f
kzVerif <165546547+kzVerif@users.noreply.github.com>
2025-08-19 22:47:19 +0700

    skip lint

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
 
-export async function GET(res: NextResponse) {
+export async function GET() {
     return Response.json({ message: "This is GET", status: 200 });
 }
