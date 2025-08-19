95aece71e5e98f40712bb2fc3f5ce5f0ab6c8ad5
kzVerif <165546547+kzVerif@users.noreply.github.com>
2025-08-19 17:58:39 +0700

    test push

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
 
 export async function GET(res: NextResponse) {
-    return Response.json({ message: "This is GET" });
+    return Response.json({ message: "This is GET", status: 200 });
 }
